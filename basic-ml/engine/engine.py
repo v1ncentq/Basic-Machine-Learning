eba8249fb47d3cf6146184a2bdcc5b03f698f172
v1ncentq <focusep12@gmail.com>
2026-08-28 16:27:27 +0300

    feat(engine): add main train loop

--- basic-ml/engine/engine.py
+++ basic-ml/engine/engine.py
@@ -62,3 +62,45 @@ def test_step(model: torch.nn.Module,
     return test_loss, test_acc
 
     
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module,
+          epochs: int,
+          device: torch.device) -> Dict[str, List]:
+
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []}
+
+    model.to(device)
+
+    for epoch in tqdm(range(epochs)):
+
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer,
+                                           device=device)
+
+        test_loss, test_acc = test_step(model=model,
+                                        dataloader=test_dataloader,
+                                        loss_fn=loss_fn,
+                                        device=device)
+
+        print(
+            f'Epoch: {epochs+1} | '
+            f'train_loss: {train_loss:.4f} | '
+            f'train_acc: {train_acc:.4f} | '
+            f'test_loss: {test_loss:.4f} | '
+            f'test_acc: {test_acc:.4f} | '
+        )
+
+        results['train_loss'].append(train_loss)
+        results['train_acc'].append(train_acc)
+        results['test_loss'].append(test_loss)
+        results['test_acc'].append(test_acc)
+
+        return results
